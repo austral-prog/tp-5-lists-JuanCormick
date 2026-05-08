b20c3298d73a2f23ff23c2d649bbb474837eeb2f
JuanCormick <juancormick28@gmail.com>
2026-05-07 21:12:58 -0300

    Add files via upload

---
+++ tp-5-lists-JuanCormick-main/tp-5-lists-JuanCormick-main/exercise_add_elements.py
@@ -0,0 +1,16 @@
+# Ejercicio 3: Agregar elementos al principio y final
+
+def add_elements(lista):
+    """
+    Agrega 'Pink' al principio y 'Yellow' al final de la lista.
+
+    Args:
+        lista: Una lista de elementos
+
+    Returns:
+        La lista modificada con los elementos agregados
+    """
+    lista.insert(0,'Pink')
+    lista.append('Yellow')
+    return lista
+
